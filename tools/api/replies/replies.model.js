1f5d058f515d9ab4b327c6b031f9b4888be385c3
TobiahRex <tobiah.rex@gmail.com>
2016-09-20 16:55:16 -0700

    finished reply model -> UNTESTED

--- tools/api/replies/replies.model.js
+++ tools/api/replies/replies.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Comment = require('../comments/commend.model');
+const User = require('../users/user.model');
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 let replySchema = new mongoose.Schema({
@@ -25,20 +26,40 @@ let replySchema = new mongoose.Schema({
 replySchema.addReply = (commentId, reply, cb) => {
   if (!commentId) return cb({ Error: 'Did not provide Comment ID for Reply.' });
   Reply.create(reply, (err1, dbReply) => {
-    if (err1) return cb({ Error: `Error making Reply: ${err1}` });
     Comment.findById(commentId, (err2, dbComment) => {
+      if (err1) return cb({ Error: `Error making Reply: ${err1}` });
       if (err2) return cb({ Error: `Could not find Comment with ID: ${commentId}` });
 
-      dbComment.replies.push(dbReply.id);
+      dbComment.replies.push(dbReply._id);
       dbComment.save((err3, savedComment) => cb(err3 || null, savedComment));
     });
   });
 };
-
-replySchema.upvote = (userId, replyId, cb) => {
+replySchema.upVote = (userId, replyId, cb) => {
   if (!userId || !replyId) return cb({ Error: 'Did not provide User Id for Upvote' });
+  User.findById(userId, (err1, dbUser) => {
+    Reply.findById(replyId, (err2, dbReply) => {
+      if (err1) return cb({ Error: `Could not find that user: ${userId}` });
+      if (err2) return cb({ Error: `Could not find that reply: ${replyId}` });
 
+      dbReply.upvotes.push(dbUser._id);
+      dbReply.save((err3, savedReply) => cb(err3 || null, savedReply));
+    });
+  });
 };
+replySchema.downVote = (userId, replyId, cb) => {
+  if (!userId || !replyId) return cb({ Error: 'Did not provide User Id for Downvote' });
+  User.findById(userId, (err1, dbUser) => {
+    Reply.findById(replyId, (err2, dbReply) => {
+      if (err1) return cb({ Error: `Could not find that user: ${userId}` });
+      if (err2) return cb({ Error: `Could not find that reply: ${replyId}` });
+
+      dbReply.downvotes.push(dbUser._id);
+      dbReply.save((err3, savedReply) => cb(err3 || null, savedReply));
+    });
+  });
+};
+
 
 const Reply = mongoose.model('Reply', replySchema);
 module.exports = Reply;
