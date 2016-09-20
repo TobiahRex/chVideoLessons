8bfb552f2de1a00c5487363625cf70b0c0eb8188
TobiahRex <tobiah.rex@gmail.com>
2016-09-19 20:17:17 -0700

    remove debug

--- tools/routes/users.js
+++ tools/routes/users.js
@@ -3,23 +3,22 @@ const router = express.Router();
 const async = require('async');
 const User = require('../models/user');
 
-router.route('/')
-.get((req, res) => User.obtainUsers(res.handle))
-.post((req, res) => User.registerNewUser(req.body, res.handle))
-.put((req, res) => User.updateUserAccount(req.body, res.handle));
+// router.route('/')
+// .get((req, res) => User.obtainUsers(res.handle))
+// .post((req, res) => User.registerNewUser(req.body, res.handle))
+// .put((req, res) => User.updateUserAccount(req.body, res.handle));
+//
+// router.get('/activeUser', User.authorization(), (req, res) =>  {
+//   let activeUser = req.user;
+//   res.send(activeUser);
+// });
+// router.delete('/deleteUser/:userId',(req, res) =>  {
+//   User.deleteUserAccount(req.params.userId,(error) =>  {
+//     if (error) res.status(400).send(error);
+//     res.send('The user has been deleted');
+//   });
+// });
 
-router.post('/login', (req, res) => User.authenticate(req.body, res.handle));
-router.delete('/logout', (req, res) => res.clearCookie('accessToken').send());
-
-router.get('/activeUser', User.authorization(), (req, res) =>  {
-  let activeUser = req.user;
-  res.send(activeUser);
-});
-router.delete('/deleteUser/:userId',(req, res) =>  {
-  User.deleteUserAccount(req.params.userId,(error) =>  {
-    if (error) res.status(400).send(error);
-    res.send('The user has been deleted');
-  });
-});
+router.route('/', )
 
 module.exports = router;
