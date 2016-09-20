ea76c1537db0eeb8a5b678f5b34d368eca93b911
TobiahRex <tobiah.rex@gmail.com>
2016-09-20 16:49:01 -0700

    built addReply method - UN-TESTED

--- tools/api/replies/replies.model.js
+++ tools/api/replies/replies.model.js
@@ -24,7 +24,20 @@ let replySchema = new mongoose.Schema({
 
 replySchema.addReply = (commentId, reply, cb) => {
   if (!commentId) return cb({ Error: 'Did not provide Comment ID for Reply.' });
-  
+  Reply.create(reply, (err1, dbReply) => {
+    if (err1) return cb({ Error: `Error making Reply: ${err1}` });
+    Comment.findById(commentId, (err2, dbComment) => {
+      if (err2) return cb({ Error: `Could not find Comment with ID: ${commentId}` });
+
+      dbComment.replies.push(dbReply.id);
+      dbComment.save((err3, savedComment) => cb(err3 || null, savedComment));
+    });
+  });
+};
+
+replySchema.upvote = (userId, replyId, cb) => {
+  if (!userId || !replyId) return cb({ Error: 'Did not provide User Id for Upvote' });
+
 };
 
 const Reply = mongoose.model('Reply', replySchema);
