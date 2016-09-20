07809e88677869bfa77b9542e7d7010d76c0d920
TobiahRex <tobiah.rex@gmail.com>
2016-09-19 20:10:47 -0700

    modified response for successfull login

--- tools/routes/login.js
+++ tools/routes/login.js
@@ -12,18 +12,10 @@ router.route('/')
     json: true
   };
   request.post(options, (err, data, body) => {
-    console.log('body: ', body);
     if (!body.token) return res.status(400).send({ Error: 'User not found' });
-    // let token = body.token.split('.')[1];
-    // console.log('token: ', token);
     JWT.verify(body.token, JWT_SECRET, (err, payload) => {
-      console.log('err: ', err);
       if (err) return res.status(401).send({ Error: 'HACKER! You are not authorized.' });
-      console.log('payload: ', payload);
-      req.user = {
-        _id: payload._id,
-        role: payload.role
-      };
+      req.user = payload;
       res.status(err ? 400 : 200).send(err || { user: req.user });
     });
   });
