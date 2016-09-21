8c2b47fd9ebb22208ea82c21ebbf677e4d1439bf
TobiahRex <tobiah.rex@gmail.com>
2016-09-21 14:08:00 -0700

    added postcomment method to Chapter model

--- tools/api/chapters/chapter.model.js
+++ tools/api/chapters/chapter.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import Comment from '../comments/comment.model';
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const chapterSchema = new mongoose.Schema({
@@ -9,6 +10,17 @@ const chapterSchema = new mongoose.Schema({
     duration: { type: Number }
   }
 });
-
+chapterSchema.postComment = (chapterId, comment, cb) => {
+  if (!chapterId || !comment) return cb({ Error: 'Missing required info to add Comment.' });
+  Chapter.findById(chapterId, (err1, dbChapter) => {
+    Comment.create(comment, (err2, dbComment) => {
+      if (err1) return cb({ Error: `Error finding Chapter: ${err1}` });
+      if (err2) return cb({ Error: `Could not create Comment - ${err2}` });
+      dbChapter.comments.push(dbComment._id);
+      dbChapter.save((err3, savedChapter) => cb(err3 || null, savedChapter));
+    });
+  });
+};
 
 const Chapter = mongoose.model('Chapter', chapterSchema);
+export default Chapter;
