57c149aa7dee85b54615773f29d4e48765e545d5
TobiahRex <tobiah.rex@gmail.com>
2016-09-21 18:53:00 -0700

    added psuedo code for creating a new lesson.

--- tools/api/lessons/lesson.model.js
+++ tools/api/lessons/lesson.model.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Type.ObjectId;
+import Chapter from '../chapters/chapter.model';
+import Section from '../sections/section.model';
+
 
 const lessonSchema = new mongoose.Schema({
   title: { type: String },
@@ -12,6 +15,35 @@ const lessonSchema = new mongoose.Schema({
 
 lessonSchema.createNewLesson = (lessonObj, cb) => {
   if (!lessonObj) return cb({ Error: 'Did not provide lesson Obj.' });
+  let chapterIDs = [];
+  lessonObj.chapters.forEach((chapter) => {
+    Chapter.create(chapter, (err1, dbChapter) => {
+      chapterIDs.push(dbChapter._id);
+    });
+  });
 
-  
+
 };
+
+/*
+  create a new cohortLesson.
+  
+  create a new lesson.
+  for each section, create a chapter.
+  save the section id and the chapter id.
+
+  insert the chapter ids into the section.
+  insert the section id into the lesson.
+
+  save the section.
+  save the lesson.
+
+
+  inside of creating each Section create a Chapter.
+  save the ids for each section and
+  create a Section for each section in the obj.
+  create a Chapter for each chapter in the obj.
+  create a lesson for the entire object.
+
+  insert the Chapter ID's their respsective
+*/
