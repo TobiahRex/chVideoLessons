796b9a67f45540e7016aecd0f5814d0f097cc89b
TobiahRex <tobiah.rex@gmail.com>
2016-09-29 14:18:54 -0700

    added reqSent method to requestsInProgress reducer. - UNTESTED

--- src/reducers/requestsInProgress.js
+++ src/reducers/requestsInProgress.js
@@ -1,22 +1,40 @@
-import * as types from "../actions/actionTypes";
-import * as initialState from "./initialState";
+// import * as types from "../actions/actionTypes";
+// import * as initialState from "./initialState";
+//
+// export default function requestStatusReducer(state = initialState.requestsInProgress, action) {
+//
+//   switch(action.type) {
+//     case types.REQUEST_SENT:
+//     return (
+//       state + 1
+//     );
+//     case types.REQUEST_RECEIVED_SUCCESSFUL:
+//     return (
+//       state - 1
+//     );
+//     case types.REQUEST_RECEIVED_ERROR:
+//     return (
+//       state - 1
+//     );
+//     default:
+//     return state;
+//   }
+// }
 
-export default function requestStatusReducer(state = initialState.requestsInProgress, action) {
+import Types from '../actions/actionTypes';
+import Immutable from 'seamless-immutable';
+import { createReducer } from 'reduxsauce';
 
-  switch(action.type) {
-    case types.REQUEST_SENT:
-    return (
-      state + 1
-    );
-    case types.REQUEST_RECEIVED_SUCCESSFUL:
-    return (
-      state - 1
-    );
-    case types.REQUEST_RECEIVED_ERROR:
-    return (
-      state - 1
-    );
-    default:
-    return state;
-  }
-}
+export const INITIAL_STATE = Immutable({
+  reqIP: 0
+});
+
+const reqSent = (state, action) =>
+state.merge(state['reqIP'] += 1);
+
+
+const ACTION_HANDLERS = {
+  [Types.REQUEST_SENT]: reqSent,
+  [Types.REQUEST_RECEIVED_SUCCESSFUL]: reqSuccess,
+  [Types.REQUEST_RECEIVED_ERROR]: reqError,
+};
