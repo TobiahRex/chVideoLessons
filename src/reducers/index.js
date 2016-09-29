1585325953a950ee0fb6853c937bef1499252f71
TobiahRex <tobiah.rex@gmail.com>
2016-09-29 14:13:21 -0700

    re-wrote rootReducer for seamless-immutable & reduxsauce - UNTESTED

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,17 @@
-import { combineReducers } from "redux";
-import initialStateReducer from "./initialStateReducer";
-import requestStatusReducer from "./requestsInProgress";
+// import { combineReducers } from "redux";
+// import initialStateReducer from "./initialStateReducer";
+// import requestStatusReducer from "./requestsInProgress";
+//
+// const rootReducer = combineReducers({
+//     initialState: initialStateReducer,
+//     requestsInProgress: requestStatusReducer
+// });
+//
+// export default rootReducer;
+
+import { combineReducers } from 'redux';
+import requestInProgress from './requestsInProgress';
 
 const rootReducer = combineReducers({
-    initialState: initialStateReducer,
-    requestsInProgress: requestStatusReducer
+  requestInProgress
 });
-
-export default rootReducer;
