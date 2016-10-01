98bb25d61db1ef8aca8d36ec0e8bbfe6b62ea7e7
TobiahRex <tobiah.rex@gmail.com>
2016-09-30 23:50:36 -0700

    added GET method to sections api - UNTESTED

--- server/api/sections/sections.js
+++ server/api/sections/sections.js
@@ -5,6 +5,7 @@ import Section from './section.model';
 router.post('/', (req, res) => Section.create(req.body, res.handle));
 
 router.route('/:id')
+.get((req, res) => Section.findById(req.params.id, res.handle))
 .put((req, res) => Section.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true }, res.handle))
 .delete((req, res) => Section.deepRemove(req.params.id, res.handle));
 
