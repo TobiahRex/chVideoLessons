7415af24cd948dc5b710f6e64c14c23b8560f891
TobiahRex <tobiah.rex@gmail.com>
2016-10-01 19:24:27 -0700

    added addComments method to chapter model.

--- server/api/chapters/chapter.model.js
+++ server/api/chapters/chapter.model.js
@@ -10,5 +10,15 @@ const chapterSchema = new mongoose.Schema({
     duration: { type: Number }
   }
 });
+// this route assumed that you have already created each Comment.
+chapterSchema.statics.addComments = (chapterId, commentIDs, cb) => {
+  if (!chapterId) return cb({ Error: 'Did not provide chapter Id.' });
+
+  Chapter.findById(chapterId).exec()
+  .then((dbChapter) => dbChapter.comments.push(commentIDs).save())
+  .then((savedChapter) => cb(null, savedChapter))
+  .catch((err) => cb(err));
+};
+
 const Chapter = mongoose.model('Chapter', chapterSchema);
 export default Chapter;
