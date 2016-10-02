23d0817b19785aba44eb775a4ce18a566fb940b2
TobiahRex <tobiah.rex@gmail.com>
2016-10-01 19:48:24 -0700

    added create route to comments api.

--- server/api/chapters/chapters.js
+++ server/api/chapters/chapters.js
@@ -2,6 +2,11 @@ import express from 'express';
 const router = express.Router();
 import Chapter from './chapter.model';
 
+router.route('/')
+.get((req, res) => Chapter.find({}, res.handle))
+.post((req, res) => Chapter.create({ title: req.body.title, vide: { url: req.body.url, duration: req.body.duration } }));
+
+
 router.route('/:id')
 .put((req, res) => Chapter.addComments(req.params.id, req.body, res.handle))
 .delete((req, res) => Chapter.findByIdAndRemove(req.params.id, res.handle));
