3c085fc60564814ccde529e6e129922e1d8179d0
TobiahRex <tobiah.rex@gmail.com>
2016-10-01 19:50:42 -0700

    changed create route to comments api.

--- server/api/chapters/chapters.js
+++ server/api/chapters/chapters.js
@@ -4,8 +4,7 @@ import Chapter from './chapter.model';
 
 router.route('/')
 .get((req, res) => Chapter.find({}, res.handle))
-.post((req, res) => Chapter.create({ title: req.body.title, vide: { url: req.body.url, duration: req.body.duration } }));
-
+.post((req, res) => Chapter.create(req.body, res.handle));
 
 router.route('/:id')
 .put((req, res) => Chapter.addComments(req.params.id, req.body, res.handle))
