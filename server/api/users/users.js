1afb52a1967305bf9f757cf31e2fb70433aa27fa
TobiahRex <tobiah.rex@gmail.com>
2016-10-02 19:27:48 -0700

    added get method for all Cohort IDs - UNTESTED

--- server/api/users/users.js
+++ server/api/users/users.js
@@ -17,7 +17,7 @@ const auth = require('../../auth/auth.middlewares');
 
 router.route('/')
 .get((req, res) => {
-  request.get('http://test.codinghouse.co/api/users/me', (err, body, data) => {
+  request.get('http://test.codinghouse.co/api/users/me', (err, res, body) => {
     return res.status(err ? 400 : 200).send(err || body);
   }).auth(null, null, true, req.headers.authorization.split(' ')[1]);
 });
@@ -25,8 +25,15 @@ router.route('/')
 router.route('/:id')
 .get(auth.isAuthenticated(), (req, res, next) => {
 
-  request.get(`http://test.codinghouse.co/api/users/${req.params.id}`, (err, data, body) => {
+  request.get(`http://test.codinghouse.co/api/users/${req.params.id}`, (err, res, body) => {
     return res.status(err ? 400 : 200).send(err || JSON.parse(body));
+  }).auth(null, null, true, req.headers.authorization.split(' ')[1]);
+});
+
+router.route('/cohorts/')
+.get(auth.isAuthenticated(), (req, res, next) => {
+  request.get(`http://test.codinghouse.co/api/cohorts/`, (err, res, body) => {
+    return res.status(err ? 400 : 200).send(err || body);    
   }).auth(null, null, true, req.headers.authorization.split(' ')[1]);
 });
 
